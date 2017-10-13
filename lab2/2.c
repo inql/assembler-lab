ae2c00970abe050ce96e4175575a378b14edcaf2
Dawid Bińkuś <dbinkus@linux-p109-8.inf.ug.edu.pl>
2017-10-13 09:51:30 +0200

    removed ecx register

--- lab2/2.c
+++ lab2/2.c
@@ -11,14 +11,12 @@ int main(void){
 
     "mov eax, %1;"
     "xor ebx, ebx;"     //zerowanie zmiennej za pomoca XOR'a
-    "xor ecx, ecx;"
     "petla:"            //stworz etykiete petla
       "shr eax;"          //przesuniecie bitowe w prawo
       "jnc skok;"         //instrukcja warunkowa - jesli flaga c nie jest zapalona przejdz do skok
       "inc ebx;"          //inkrementacja ebx
       "skok:"
-        "inc ecx;"
-        "cmp ecx,32;"     //porownaj licznik z liczba 32
+        "cmp eax,0;"      //porownaj eax z 0
         "jnz petla;"      //jesli flaga z nie jest zapalona przejdz do petla
       "mov %0, ebx;"
     ".att_syntax prefix"
